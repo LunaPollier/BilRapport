3d5615a2941cbea458768e6e52a75b8d525ea99b
lunapollier <lupo0001@stud.kea.dk>
2024-05-02 18:25:11 +0200

    oprettet

---
+++ src/main/java/com/kea/bilrapport/BilRapportApplication.java
@@ -0,0 +1,13 @@
+package com.kea.bilrapport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilRapportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilRapportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kea/bilrapport/BilRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kea.bilrapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilRapportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

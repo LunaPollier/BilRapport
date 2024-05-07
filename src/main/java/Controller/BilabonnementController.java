e98a6c922e3b0b2b9f7b085fad87a28937786e6d
lunapollier <lupo0001@stud.kea.dk>
2024-05-07 12:31:59 +0200

    igang med login til medarbejdere

---
+++ src/main/java/Controller/BilabonnementController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BilabonnementController{
+
+}
---
+++ src/main/java/Controller/opretcontroller.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class opretcontroller {
+    @GetMapping("/opret")
+    public String showopretForm() {
+        return "Opret";
+    }
+
+    @PostMapping("/submit-your-opret-path")
+    public String submitSignupForm(@RequestParam("username") String username,
+                                   @RequestParam("email") String email,
+                                   @RequestParam("password") String password,
+                                   @RequestParam("confirm-password") String confirmPassword) {
+
+        System.out.println("Modtaget brugeroprettelsesdata:");
+        System.out.println("Brugernavn: " + username);
+        System.out.println("E-mail: " + email);
+        System.out.println("Adgangskode: " + password);
+        System.out.println("Bekræft adgangskode: " + confirmPassword);
+
+
+        return "opret-success";
+    }
+}
+

05185da5f7125e1eaa61e60ff9005c99725c902a
lunapollier <lupo0001@stud.kea.dk>
2024-05-22 11:39:05 +0200

    lejecontroller opdateret

--- src/main/java/Controller/Lejeaftalecontroller.java
+++ src/main/java/Controller/Lejeaftalecontroller.java
@@ -1,34 +1,42 @@
 package Controller;
-import Klasser.Lejeaftale;
-import Klasser.LejeaftaleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+@RequestMapping("/lejeaftaler")
+public class Lejeaftalecontroller {
 
 
-    @Controller
-    @RequestMapping("/lejeaftaler")
-    public class Lejeaftalecontroller {
-
-        private final LejeaftaleService lejeaftaleService;
-
-        @Autowired
-        public Lejeaftalecontroller(LejeaftaleService lejeaftaleService) {
-            this.lejeaftaleService = lejeaftaleService;
-        }
-
-        @GetMapping("/opret")
-        public String visOpretLejeaftaleForm(Model model) {
-            model.addAttribute("lejeaftale", new Lejeaftale());
-            return "dataregistrering";
-        }
-
-        @PostMapping("/opret")
-        public String opretLejeaftale(@ModelAttribute Lejeaftale lejeaftale, Model model) {
-            lejeaftaleService.gemLejeaftale(lejeaftale); // Gem lejeaftalen ved hjælp af serviceklassen
-            model.addAttribute("lejeaftale", lejeaftale);
-            return "opret-lejeaftale-success"; // Retur stien uden ".html"
-        }
+    @GetMapping("/opret")
+    public String visOpretLejeaftaleForm(Model model) {
+        model.addAttribute("lejeaftale", new Lejeaftale()); // Tilføjer en ny lejeaftale til modellen
+        return "opret-lejeaftale"; // Returnerer navnet på HTML-filen, der indeholder formularen
     }
+
+    @PostMapping("/opret")
+    public String opretLejeaftale(@ModelAttribute Lejeaftale lejeaftale, Model model) {
+        // Gem lejeaftalen i databasen eller udfør anden relevant logik
+        // Her kan du indsætte din kode til at gemme lejeaftalen i databasen
+        // f.eks. ved hjælp af en serviceklasse eller et repository
+
+        // Eksempel: lejeaftaleService.gemLejeaftale(lejeaftale);
+
+        // Tilføj lejeaftalen til modellen, hvis du vil vise den på bekræftelsessiden
+        model.addAttribute("lejeaftale", lejeaftale);
+
+        // Returner navnet på HTML-filen til bekræftelsessiden
+        return "opret-lejeaftale-success";
+    }
+
+
+    // GET-mapping for at vise en bekræftelsesside efter vellykket oprettelse af lejeaftalen
+    @GetMapping("/opret-success")
+    public String visOpretSuccessSide() {
+        return "opret-lejeaftale-success"; // Returnerer navnet på HTML-filen for bekræftelsessiden
+    }
+
+    private class Lejeaftale {
+    }
+}
 
